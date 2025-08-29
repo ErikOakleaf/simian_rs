c344a11e131cb0473f42e4558e09a9fc32aebee4
Erik Ekblad <oakl33f123@gmail.com>
2025-08-29 14:31:39 +0200

    added evaluting bang prefix expressions rewrote functions to not return results for now

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,4 +1,4 @@
-use crate::ast::{Expression, Program, Statement};
+use crate::ast::{Expression, PrefixExpression, Program, Statement};
 use crate::lexer::Lexer;
 use crate::object::Object;
 use crate::parser::{ParseError, Parser};
@@ -8,38 +8,66 @@ pub enum EvaluationError {
     GenericError,
 }
 
-pub fn eval_program(program: &Program) -> Result<Object, EvaluationError> {
+pub fn eval_program(program: &Program) -> Object {
     eval_statements(&program.statements)
 }
 
-fn eval_statements(statements: &[Statement]) -> Result<Object, EvaluationError> {
+fn eval_statements(statements: &[Statement]) -> Object {
     let mut result = Object::Null;
     for statement in statements {
-        result = eval_statement(&statement)?;
+        result = eval_statement(&statement);
     }
-    Ok(result)
+    result
 }
 
-fn eval_statement(statement: &Statement) -> Result<Object, EvaluationError> {
+fn eval_statement(statement: &Statement) -> Object {
     match statement {
         Statement::Expression(expression_statement) => {
             eval_expression(&expression_statement.expression)
         }
-        _ => Err(EvaluationError::GenericError),
+        _ => Object::Null 
     }
 }
 
-fn eval_expression(expression: &Expression) -> Result<Object, EvaluationError> {
+fn eval_expression(expression: &Expression) -> Object {
     match expression {
         Expression::IntegerLiteral(integer_literal_expression) => {
-            Ok(Object::Integer(integer_literal_expression.value))
+            Object::Integer(integer_literal_expression.value)
         }
-        Expression::Boolean(boolean_expression) => {
-            Ok(Object::Boolean(boolean_expression.value))
+        Expression::Boolean(boolean_expression) => Object::Boolean(boolean_expression.value),
+        Expression::Prefix(prefix_expression) => {
+            let right = eval_expression(&prefix_expression.right);
+            eval_prefix_expression(&prefix_expression.token.literal, &right)
         }
-        _ => Err(EvaluationError::GenericError),
+
+
+        _ =>  Object::Null
     }
 }
+
+fn eval_prefix_expression(operator: &str, right: &Object) -> Object {
+    match operator {
+        "!" => {
+            eval_bang_expression(right)
+        }
+        _ => {
+            Object::Null
+        }
+    }
+}
+
+fn eval_bang_expression(right: &Object) -> Object {
+    match right {
+        Object::Boolean(bool) => {
+           Object::Boolean(!bool)
+        }
+        Object::Null => {
+            Object::Boolean(true)
+        }
+        _ => Object::Boolean(false)
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -55,7 +83,7 @@ mod tests {
         parser.parse_program()
     }
 
-    fn test_eval(input: &str) -> Result<Object, EvaluationError> {
+    fn test_eval(input: &str) -> Object {
         let program = parse_input(input).expect("Parsing failed");
         eval_program(&program)
     }
@@ -91,20 +119,40 @@ mod tests {
         let tests: [(&str, i64); 2] = [("5", 5), ("10", 10)];
 
         for (input, expected) in tests {
-            let evaluated = test_eval(input)?;
+            let evaluated = test_eval(input);
             test_integer_object(evaluated, expected);
         }
 
         Ok(())
     }
 
-
     #[test]
     fn test_eval_boolean_expression() -> Result<(), EvaluationError> {
         let tests: [(&str, bool); 2] = [("true", true), ("true", true)];
 
         for (input, expected) in tests {
-            let evaluated = test_eval(input)?;
+            let evaluated = test_eval(input);
+            test_boolean_object(evaluated, expected);
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_bang_operator() -> Result<(), EvaluationError> {
+        let tests: [(&str, bool); 8] = [
+            ("true", true),
+            ("true", true),
+            ("!true", false),
+            ("!false", true),
+            ("!5", false),
+            ("!!true", true),
+            ("!!false", false),
+            ("!!5", true),
+        ];
+
+        for (input, expected) in tests {
+            let evaluated = test_eval(input);
             test_boolean_object(evaluated, expected);
         }
 
